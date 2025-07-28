8a487d53a7ce807cc1f9bd3d0e930d46c1a76efb
konradfoulk <konrad.foulk@gmail.com>
2025-07-28 19:53:26 -0400

    basic foundation

---
+++ react_site/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div id="header">
+            <h1>Konrad Foulk, Jacksonville FL</h1>
+            <a href="">Email</a><a href="">LinkedIn</a><a href="">GitHub</a>
+        </div>
+    );
+};
+
+export default Header
---
+++ react_site/src/components/TagBtn.jsx
@@ -0,0 +1,8 @@
+function TagBtn() {
+
+    return (
+        <a id="tag-btn" href="../assets/resume.pdf" download='CV_Konrad_Foulk.pdf'>RESUME</a>
+    )
+};
+
+export default TagBtn
