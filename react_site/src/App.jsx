efc1def86e88c5f74d37736c4b3bac8ca2c0ce50
konradfoulk <konrad.foulk@gmail.com>
2025-07-29 10:26:00 -0400

    fade in/out

--- react_site/src/App.jsx
+++ react_site/src/App.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react'
 import TagBtn from './components/TagBtn.jsx'
 import Header from './components/Header.jsx'
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const scrollState = () => {
+      setVisible(window.scrollY > 0);
+    };
+    window.addEventListener('scroll', scrollState);
+    return () => window.removeEventListener('scroll', scrollState);
+  }, []);
 
   return (
     <>
       <TagBtn />
       <Header />
-      <div id='content'>
+      <div id='content' className={visible ? 'visible' : ''}>
         <p>aaaaaa</p>
         <p>aaaaaa</p>
         <p>aaaaaa</p>
---
+++ react_site/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+
+    return (
+        <div id="nav">
+            <a href="#header">Konrad Foulk</a>
+            <a href="#projects">Projects</a>
+            <a href="#about">About</a>
+            <a href="#skills">Skills</a>
+        </div>
+    );
+}
