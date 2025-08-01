6c00a1da78322c68964780f48290b72699d4c838
konradfoulk <konrad.foulk@gmail.com>
2025-08-01 12:41:30 -0400

    making links work

--- react_site/src/components/Footer.jsx
+++ react_site/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer>
             <div>
-                <p>Konrad Foulk</p>
+                <p>Let's see Paul Allen's card.</p>
                 <p>2025</p>
             </div>
         </footer>
--- react_site/src/components/Header.jsx
+++ react_site/src/components/Header.jsx
@@ -4,7 +4,10 @@ function Header() {
     return (
         <div id="header">
             <h1>Konrad Foulk, Jacksonville FL</h1>
-            <a href="">Email</a><a href="">LinkedIn</a><a href="">GitHub</a>
+            <a href="mailto:konrad.foulk@gmail.com" target="_blank"
+                rel="noopener noreferrer">Email</a><a href="https://www.linkedin.com/in/konrad-foulk-931934355/" target="_blank"
+                    rel="noopener noreferrer">LinkedIn</a><a href="https://github.com/konradfoulk" target="_blank"
+                        rel="noopener noreferrer">GitHub</a>
         </div>
     );
 };
