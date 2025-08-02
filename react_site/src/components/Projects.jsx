8f827b8ef92bab44f416ab63f7c4d376da93b64a
konradfoulk <konrad.foulk@gmail.com>
2025-08-02 11:31:37 -0400

    video previews

--- react_site/src/components/Projects.jsx
+++ react_site/src/components/Projects.jsx
@@ -1,4 +1,7 @@
 import './Projects.css'
+import notesAppVideo from '../assets/notes_app.mp4'
+import jobFinderVideo from '../assets/job_finder.mp4'
+import taxCalculatorVideo from '../assets/tax_calculator.mp4'
 
 function Projects() {
 
@@ -10,21 +13,27 @@ function Projects() {
                     <a href="#" className="link">Notes App</a>
                     <p className="description">Rich text editor clone of iCloud Notes and Notion</p>
                 </div>
-                <div className="preview"></div>
+                <div className="preview">
+                    <video src={notesAppVideo} autoPlay muted loop></video>
+                </div>
             </div>
             <div className="project element">
                 <div className="wrapper">
                     <a href="#" className="link">Job Finder</a>
                     <p className="description">Query APIs to output a list of job postings to a CSV</p>
                 </div>
-                <div className="preview"></div>
+                <div className="preview">
+                    <video src={jobFinderVideo} autoPlay muted loop></video>
+                </div>
             </div>
             <div className="project element">
                 <div className="wrapper">
                     <a href="#" className="link">Tax Calculator</a>
                     <p className="description">Find the income required to take home an inputted desired income after tax</p>
                 </div>
-                <div className="preview"></div>
+                <div className="preview">
+                    <video src={taxCalculatorVideo} autoPlay muted loop></video>
+                </div>
             </div>
             <div className="project element">
                 <div className="wrapper">
