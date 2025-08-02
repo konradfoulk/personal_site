8f951fa146f72e141d1c8e6012871e69ae2edb85
konradfoulk <konrad.foulk@gmail.com>
2025-08-02 11:42:44 -0400

    links

--- react_site/src/components/Projects.jsx
+++ react_site/src/components/Projects.jsx
@@ -10,7 +10,8 @@ function Projects() {
             <p className="heading">Projects</p>
             <div className="project element">
                 <div className="wrapper">
-                    <a href="#" className="link">Notes App</a>
+                    <a href="https://konradfoulk.pythonanywhere.com/" className="link" target="_blank"
+                        rel="noopener noreferrer">Notes App</a>
                     <p className="description">Rich text editor clone of iCloud Notes and Notion</p>
                 </div>
                 <div className="preview">
@@ -19,7 +20,8 @@ function Projects() {
             </div>
             <div className="project element">
                 <div className="wrapper">
-                    <a href="#" className="link">Job Finder</a>
+                    <a href="https://github.com/konradfoulk/job_finder" className="link" target="_blank"
+                        rel="noopener noreferrer">Job Finder</a>
                     <p className="description">Query APIs to output a list of job postings to a CSV</p>
                 </div>
                 <div className="preview">
@@ -28,7 +30,8 @@ function Projects() {
             </div>
             <div className="project element">
                 <div className="wrapper">
-                    <a href="#" className="link">Tax Calculator</a>
+                    <a href="https://github.com/konradfoulk/tax_calculator" className="link" target="_blank"
+                        rel="noopener noreferrer">Tax Calculator</a>
                     <p className="description">Find the income required to take home an inputted desired income after tax</p>
                 </div>
                 <div className="preview">
@@ -37,7 +40,8 @@ function Projects() {
             </div>
             <div className="project element">
                 <div className="wrapper">
-                    <a href="#" className="link">Portfolio Site</a>
+                    <a href="https://github.com/konradfoulk/personal_site" className="link" target="_blank"
+                        rel="noopener noreferrer">Portfolio Site</a>
                     <p className="description">Simple, clear, effective, me. Inspired by Paco, Seyit, and Azuki</p>
                 </div>
                 <div className="preview"></div>
