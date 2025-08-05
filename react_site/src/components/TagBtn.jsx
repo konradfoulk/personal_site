2ad8b0805ac3c0b00e1646c9e458e7066dbb0dbd
konradfoulk <konrad.foulk@gmail.com>
2025-08-05 18:55:06 -0400

    point to g drive file

--- react_site/src/components/TagBtn.jsx
+++ react_site/src/components/TagBtn.jsx
@@ -3,7 +3,7 @@ import './TagBtn.css'
 function TagBtn() {
 
     return (
-        <a id="tag-btn" href='https://drive.google.com/file/d/1qMSaLQqsIa5KcJUzTQ-X89zf8TfGkpLX/view?usp=sharing' target="_blank"
+        <a id="tag-btn" href='https://drive.google.com/file/d/1nqPG3_7Lo55J23zVz96_n74v1l7pWMry/view?usp=sharing' target="_blank"
             rel="noopener noreferrer">RESUME</a>
     )
 };
