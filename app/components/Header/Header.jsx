5d1898e9d830982253dce35a6f29cb10bb6fe49f
konradfoulk <konrad.foulk@gmail.com>
2025-08-08 12:30:44 -0400

    update links

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div id="header">
             <h1>Konrad Foulk, Jacksonville FL</h1>
             <a href="mailto:konrad.foulk@gmail.com" target="_blank"
-                rel="noopener noreferrer">Email</a><a href="https://www.linkedin.com/in/konrad-foulk-931934355/" target="_blank"
+                rel="noopener noreferrer">Email</a><a href="https://www.linkedin.com/in/konradfoulk/" target="_blank"
                     rel="noopener noreferrer">LinkedIn</a><a href="https://github.com/konradfoulk" target="_blank"
                         rel="noopener noreferrer">GitHub</a>
         </div>
