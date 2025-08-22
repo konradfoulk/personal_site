3b75c2dc8813e831f557ae0228eb0879a50af33f
konradfoulk <konrad.foulk@gmail.com>
2025-08-22 12:46:31 -0400

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <section id="about">
-            <p className="text-element">Aspiring Software Engineer</p>
+            <p className="text-element">Software Engineer</p>
             <p className="text-element"><em>I love creating systems.</em> Extracting maximum efficiency from everything I see is my obsession. Programming is my favorite medium to turn my ideas into real-world applications.</p>
             <p className="text-element">I focus on three core principals; automate with CS fundementals to streamline tasks, share to the web so projects can be used and improved, and integrate others work using APIs to expand capabilities.</p>
             <p className="text-element">I am happiest when I am increasing my ability to do things. My dream is an occupation that keeps me learning new skills around my passions, and connects me with like-minded people.</p>
