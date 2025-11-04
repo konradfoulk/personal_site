43b36e5534b972aa4c34e19ba0b88c7c1a454cca
konradfoulk <konrad.foulk@gmail.com>
2025-11-04 10:10:21 -0500

    updating information

--- app/components/Skills/Skills.jsx
+++ app/components/Skills/Skills.jsx
@@ -4,10 +4,11 @@ export default function Skills() {
     return (
         <section id="skills">
             <p className="heading"> Skills</p>
-            <p className='text-element'>My expertise spans prototyping tools and full-stack web tech, <em>earned through hundreds of hours of study and hands-on experience.</em> In the future, I want to build optimized, high performance scalable applications and learn Rust.</p>
+            <p className='text-element'>My expertise spans prototyping tools and full-stack web tech, <em>earned from over a thousand hours of study and hands-on experience.</em> In the future, I aim to leverage my expertise in building robust full stack applications with Go to transform my projects into businesses of their own.</p>
             <p className='text-element'>Below is the core toolkit I use to make my ideas happen.</p>
             <div id="marquee" className='fadeout-horizontal'>
                 <div id="marquee-track">
+                    <span>Go</span>
                     <span>Python</span>
                     <span>Javascript</span>
                     <span>Flask</span>
@@ -16,6 +17,7 @@ export default function Skills() {
                     <span>SQL</span>
                     <span>HTML</span>
                     <span>CSS</span>
+                    <span>Go</span>
                     <span>Python</span>
                     <span>Javascript</span>
                     <span>Flask</span>
@@ -24,6 +26,7 @@ export default function Skills() {
                     <span>SQL</span>
                     <span>HTML</span>
                     <span>CSS</span>
+                    <span>Go</span>
                     <span>Python</span>
                     <span>Javascript</span>
                     <span>Flask</span>
@@ -32,6 +35,7 @@ export default function Skills() {
                     <span>SQL</span>
                     <span>HTML</span>
                     <span>CSS</span>
+                    <span>Go</span>
                     <span>Python</span>
                     <span>Javascript</span>
                     <span>Flask</span>
--- app/components/TagBtn/TagBtn.jsx
+++ app/components/TagBtn/TagBtn.jsx
@@ -2,7 +2,7 @@ import './TagBtn.css'
 
 export default function TagBtn() {
     return (
-        <a id="tag-btn" href='https://drive.google.com/file/d/1QFJj3svKP-geERnncTb5ciMnuA3Tchi5/view?usp=sharing' target="_blank"
+        <a id="tag-btn" href='https://drive.google.com/file/d/1hsjKlxI-jUKrYLPmO9Ec5HtLXv7_KUmh/view?usp=sharing' target="_blank"
             rel="noopener noreferrer">RESUME</a>
     )
 }
