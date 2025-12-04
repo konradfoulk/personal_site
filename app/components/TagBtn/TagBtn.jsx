d6ba16dc372bf1271b04843df2ef03b75d451a4a
konradfoulk <konrad.foulk@gmail.com>
2025-12-04 16:46:37 -0500

    new resume

--- app/components/TagBtn/TagBtn.jsx
+++ app/components/TagBtn/TagBtn.jsx
@@ -2,7 +2,7 @@ import './TagBtn.css'
 
 export default function TagBtn() {
     return (
-        <a id="tag-btn" href='https://drive.google.com/file/d/1hsjKlxI-jUKrYLPmO9Ec5HtLXv7_KUmh/view?usp=sharing' target="_blank"
+        <a id="tag-btn" href='https://drive.google.com/file/d/1LW8UfNehxNs5cNNG6cJcnVFeJUzlYgaE/view?usp=sharing' target="_blank"
             rel="noopener noreferrer">RESUME</a>
     )
 }
